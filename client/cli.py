9e137c8d43fdcfe1f885b881b3e3461012e0e49f
jorgesece <jorgesece@lip.pt>
2016-01-26 12:32:53 +0000

    add sub commands to the client manager

--- client/cli.py
+++ client/cli.py
@@ -1,10 +1,48 @@
 import click
 
 
-@click.command()
-@click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
-@click.argument('name', default='world', required=False)
-def main(name, as_cowboy):
-    """Python openstack client which interacts with the OS restful api directly"""
-    greet = 'Howdy' if as_cowboy else 'Hello'
-    click.echo('{0}, {1}.'.format(greet, name))
+
+@click.group()
+@click.version_option()
+def openstack():
+    """Openstack restful client which directly uses the REST ful api of OS.
+    """
+
+@openstack.group()
+def project():
+    """Manages ships."""
+
+
+@project.command('create')
+@click.argument('name')
+@click.option('--description', '-d', help='Description of the project.')
+def project_create(name):
+    """Creates a new ship."""
+    click.echo('NOT IMPLEMENTED. Created project %s' % name)
+
+
+@project.command('createBunch')
+@click.argument('file', help='JSON file with project information')
+def project_create(file):
+    """Creates a new ship."""
+    click.echo('NOT IMPLEMENTED. Created project from a JSON file %s' % file)
+
+
+@openstack.group()
+def user():
+    """Manages ships."""
+
+
+@user.command('create')
+@click.argument('name')
+@click.option('--description', '-d', help='Description of the user.')
+def user_create(name):
+    """Creates a new ship."""
+    click.echo('NOT IMPLEMENTED. Created user %s' % name)
+
+
+@user.command('createBunch')
+@click.argument('file', help='JSON file with user information')
+def user_create(file):
+    """Creates a new ship."""
+    click.echo('NOT IMPLEMENTED. Created user from a JSON file %s' % file)
